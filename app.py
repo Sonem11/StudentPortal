e6078f050be2eb63fd27f24f0b06476cefbe8431
Sonem11 <miljkovicnnd@gmail.com>
2026-04-27 20:32:29 +0200

    Add /create_admin route for admin role on Render

--- app.py
+++ app.py
@@ -154,6 +154,16 @@ def delete_student(id):
 # ---------------------------
 # CREATE ADMIN ROUTE (privremeno za Render)
 # ---------------------------
+# ---------------------------
+# CREATE ADMIN ROUTE (privremeno za Render)
+# ---------------------------
+@app.route("/create_admin")
+def create_admin():
+    cursor = db.conn.execute("SELECT * FROM users WHERE username=?", ("admin",))
+    if cursor.fetchone():
+        return "Admin already exists!"
+    db.insert_user("admin", "admin123", role="admin")
+    return "Admin created: username=admin, password=admin123"
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
