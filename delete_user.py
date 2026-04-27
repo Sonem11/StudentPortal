6862b86cbd64c53d9c68aeaab6acfeb768b1d20d
Sonem11 <miljkovicnnd@gmail.com>
2026-04-27 20:22:03 +0200

    Remove a temporary admin/user scripts

--- delete_user.py
+++
@@ -1,8 +0,0 @@
-from database import Database
-
-db = Database("students.db")
-
-db.conn.execute("DELETE FROM users WHERE username=?", ("admin",))
-db.conn.commit()
-
-print("User 'admin' deleted.")
